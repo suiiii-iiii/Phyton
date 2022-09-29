b082d8c89c1987beec7c111ff58b387f666508b8
movavi_school <Makleplin@gmail.com>
2022-09-29 12:04:49 +0700

    прошёл второй урок

---
+++ lesson 2/main.py
@@ -0,0 +1,50 @@
+
+# a = int(input('a: '))
+# first = a // 100
+# second = a // 10 % 10
+# tretie = a % 10
+# print("последняя цифра", a % 10)
+# print("первая цифра", a // 100)
+# print("вторая цифра", a // 10 % 10)
+# print("произведение цифр числа =", first * second * tretie )
+
+
+# a = int(input("a: "))
+# if a % 2 == 1:
+#  a = a + 1
+# else:
+#  a = a + 2
+# print(a)
+
+
+# d = int(input('d: '))
+# r = d/2
+# print("Длинна =", d * 3.14)
+# print("Площадь =",3.14 * r ** 2)
+
+
+# n = int(input('sec: '))
+#
+# h = n // 3600
+#
+# m = n % 3600 // 60
+#
+# s = n % 3600 % 60 % 60
+#
+# if h < 10:
+#  h = "0" + str(h)
+#
+#  if m < 10:
+#   h = "0" + str(m)
+#
+# if s < 10:
+#  h = "0" + str(s)
+#
+# print(f"{h}:{m}:{s}")
+
+
+
+
+
+
+
